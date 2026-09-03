cd5ade17c9400c6d8cf1616b9591f5571360272e
Buisness93 <6cm22829b5@privaterelay.appleid.com>
2026-09-03 16:34:37 +0200

    Agrandit encore le camion bleu a remorque du traffic

--- game/js/engine.js
+++ game/js/engine.js
@@ -25,7 +25,7 @@
     { file:'../uploads/traffic-bus-grey.gltf', len:8.0 },
     { file:'../uploads/traffic-bus-white.glb', len:9.0 },
     { file:'../uploads/traffic-truck-semi.glb', len:10.0 },
-    { file:'../uploads/traffic-truck-flatbed.gltf', len:9.0 },
+    { file:'../uploads/traffic-truck-flatbed.gltf', len:11.5 },
   ];
 
   // Fantome du meilleur run (par voiture+route) : stocke localement, aucun
